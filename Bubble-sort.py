4e99f8e887d37434a0d6f2f5315367f4e687be40
shahzoorkhan <lamborghinia693@gmail.com>
2022-05-21 22:19:15 +0500

    Solving Bubble sort problem name sort color with comprehension technique

---
+++ Bubble-sort.py
@@ -0,0 +1,6 @@
+nums = [2,0,2,1,1,0]
+
+nums = [ ()  for i in nums for j in nums if i > j ]
+
+print(nums)
+
---
+++ test.py
@@ -0,0 +1,22 @@
+from tempfile import tempdir
+
+
+nums = [2,0,2,1,1,0]
+
+if nums[0] > nums[1]:
+
+    nums = nums[1] == nums[0]
+    print(nums)
+
+
+
+# for i in nums:
+#     for j in nums:
+#         if i > j + 1 :
+#             temp = 0
+#             nums[i] == temp
+#             temp = nums[j]
+#             nums += temp
+#             print (nums)
+
+#             # print(nums)
