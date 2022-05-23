94605010c659a2a3379fa97f1636c22827030869
shahzoorkhan <lamborghinia693@gmail.com>
2022-05-23 23:48:48 +0500

    first drop is not always easy

--- test.py
+++ test.py
@@ -5,10 +5,5 @@ nums = [2,0,2,1,1,0]
 for i in nums:
     for j in nums:
         if i > j + 1 :
-            temp = 0
-            nums[i] == temp
-            temp = nums[j]
-            
+            nums =  nums[i] == nums[j]
             print (nums)
-
-            # print(nums)
