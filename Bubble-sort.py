2f56d31fe3426d7e567ae8ad0985e70f852c2e63
shahzoorkhan <lamborghinia693@gmail.com>
2022-05-24 20:31:42 +0500

    solving errors

--- Bubble-sort.py
+++ Bubble-sort.py
@@ -1,6 +1,12 @@
 nums = [2,0,2,1,1,0]
 
-nums = [ ()  for i in nums for j in nums if i > j ]
+
+if nums[0] > nums[1]:
+    nums = len(nums) - 2
 
 print(nums)
+
+# nums = [ ()  for i in nums for j in nums if i > j ]
+
+# print(nums)
 
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 nums = [2,0,2,1,1,0]
-
-
-
 for i in nums:
     for j in nums:
         if i > j + 1 :
-            nums =  nums[i] == nums[j]
-            print (nums)
+            print(i)
+            print(nums)
+            
+          
+
